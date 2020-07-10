da28d619c61cb100fe33be71728a52cd65b13432
is.kudryavcev <is.kudryavcev@tensor.ru>
2020-07-10 11:18:21 +0300

    https://online.sbis.ru/opendoc.html?guid=06b95354-2c3d-4eb2-9f55-62788ac12273

--- app/app.js
+++ app/app.js
@@ -123,36 +123,6 @@ function serverSideRender(req, res) {
    setDebugCookie(req, res);
 }
 
-function loadConfiguration(req, res) {
-   requirejs(['I18n/i18n'], (i18n) => {
-      const locale = req.query.locale || req.cookies.lang;
-      i18n.Loader.loadConfiguration(locale).then((configuration) => {
-         if (typeof req.query.v !== 'undefined') {
-            res.set('Cache-Control', 'public, max-age=315360000, immutable');
-         }
-         res.json(configuration);
-      }, (err) => {
-         res.status(404).send(err);
-      });
-   });
-}
-
-function loadDictionary(req, res) {
-   requirejs(['Core/i18n/Loader'], (Loader) => {
-      const module = req.query.module;
-      const locale = req.query.locale || req.cookies.lang;
-
-      Loader.default.dictionary(module, locale).then((dictionary) => {
-         if (typeof req.query.v !== 'undefined') {
-            res.set('Cache-Control', 'public, max-age=315360000, immutable');
-         }
-         res.json(dictionary);
-      }, (err) => {
-         res.status(404).send(err);
-      });
-   });
-}
-
 function setDebugCookie(req, res) {
    if (req.cookies.s3debug === undefined) {
       res.cookie('s3debug', true, { maxAge: 900000, httpOnly: true });
