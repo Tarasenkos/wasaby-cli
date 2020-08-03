eb44b6121598a25632e9368b61a91492ab219f7a
ganshinyao <yo.ganshin@tensor.ru>
2020-08-03 10:19:40 +0300

    https://online.sbis.ru/opendoc.html?guid=306df573-4e85-4da4-8fe0-2a24c013e5f2

--- app/xml/project.js
+++ app/xml/project.js
@@ -4,9 +4,12 @@ const fsUtil = require('../util/fs');
 const fs = require('fs-extra');
 
 function getUiModules(srv) {
-   if (!srv.service.items[0].ui_module) {
+   if (typeof srv.service.items[0] !== 'object') {
       srv.service.items[0] = {ui_module: []}
+   } else {
+      srv.service.items[0].ui_module = [];
    }
+
    return srv.service.items[0].ui_module;
 }
 
