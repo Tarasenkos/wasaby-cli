f12a2f97061061fdc076278e2a4798dbf3c92443
ganshinyao <yo.ganshin@tensor.ru>
2020-10-28 14:28:23 +0300

    link on local repos https://online.sbis.ru/opendoc.html?guid=b0cfcd8c-44e0-4a19-9750-d43218f26c6a

--- app/store.js
+++ app/store.js
@@ -79,13 +79,16 @@ class Store extends Base {
          path: path.join(this._store, name),
          name: name
       });
-
       let [branch, mergeWith] = commit.split(':');
       const isBranch = Git.isBranch(branch);
 
-      logger.log(`Переключение на ветку ${branch}`, name);
       await git.update();
       if (isBranch) {
+         if (commit.includes('rc-')) {
+            branch = await git.getNearestRcBranch(commit);
+         }
+
+         logger.log(`Переключение на ветку ${branch}`, name);
          try {
             await git.checkout(branch);
          } catch (err) {
