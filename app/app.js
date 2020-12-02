e7b4e4404b2c6e12dbca252d7ce88400c83385eb
mi.hambelov <mi.hambelov@tensor.ru>
2020-12-02 14:08:14 +0300

    https://online.sbis.ru/opendoc.html?guid=5cee648e-f949-42b0-a5a4-c8e12e7e5806

--- app/app.js
+++ app/app.js
@@ -44,13 +44,13 @@ async function run(resources, port, config) {
    console.log('start init');
 
    const ready = new Promise((resolve, reject) => {
-      requirejs(['Env/Env', 'Application/Initializer', 'SbisEnv/PresentationService', 'Application/State', 'Core/core-init'], function(Env, AppInit, PS,  AppState) {
+      requirejs(['Env/Env', 'Application/Initializer', 'SbisEnv/PresentationService', 'Application/State', 'Core/core-init', 'UI/State'], function(Env, AppInit, PS,  AppState, UIState) {
          Env.constants.resourceRoot = resourceRoot;
          Env.constants.modules = requirejs('json!/contents').modules;
 
          if (!AppInit.isInit()) {
             // eslint-disable-next-line new-cap
-            AppInit.default({ resourceRoot }, new PS.default({ resourceRoot }), new AppState.StateReceiver());
+            AppInit.default({ resourceRoot }, new PS.default({ resourceRoot }), new AppState.StateReceiver(UIState.Serializer));
          }
 
          console.log(`server started http://localhost:${availablePort}`);
