ee69bce04e40dadd184c40e17d01f0de4c3850a5
Кудрявцев И.С <is.kudryavcev@corp.tensor.ru>
2020-12-08 13:25:57 +0300

    https://online.sbis.ru/opendoc.html?guid=1ac986f6-53b3-4ca9-9e76-59d6870d5b42

--- test/net/getPort.test.js
+++ test/net/getPort.test.js
@@ -2,11 +2,11 @@ const chai = require('chai');
 const getPort = require('../../app/net/getPort');
 
 describe('getPort', () => {
-   it('should return avaliable port', async() => {
+   it('should return available port', async() => {
       chai.expect(await getPort()).to.be.within(1023,65536);
    });
 
-   it('should return avaliable port', async() => {
+   it('should return available port', async() => {
       chai.expect(await getPort()).to.be.above(1023).and.to.be.below(65536);
    });
 });
